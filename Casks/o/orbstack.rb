c46ce59af8a111fdc3a7cd674c859a61d3661e51
Felipe Barbosa do Nascimento <dfelipebdn@gmail.com>
2024-06-19 08:36:18 -0300

    Add orbstack version 1.1.0

---
+++ Casks/o/orbstack.rb
@@ -0,0 +1,52 @@
+cask "orbstack" do
+  arch arm: "arm64", intel: "amd64"
+
+  version "1.1.0_16370"
+  sha256 arm:   "f6619eb2d2135e02f64310191462ae69f12b6da70cc2d6d4e656ca621749ba61",
+         intel: "bd6c5b10d01dfbc18afc997ed1dd3fe8c89e3bb75674094a55ed8f996ad771de"
+
+  url "https://cdn-updates.orbstack.dev/#{arch}/OrbStack_v#{version}_#{arch}.dmg"
+  name "OrbStack"
+  desc "Replacement for Docker Desktop"
+  homepage "https://orbstack.dev/"
+
+  livecheck do
+    url "https://orbstack.dev/download/beta/latest/#{arch}"
+    regex(/OrbStack[._-]v?(\d+(?:[._-]\d+)+)[._-]#{arch}\.dmg/i)
+    strategy :header_match
+  end
+
+  auto_updates true
+  depends_on macos: ">= :monterey"
+
+  app "OrbStack.app"
+  binary "#{appdir}/OrbStack.app/Contents/MacOS/bin/orb"
+  binary "#{appdir}/OrbStack.app/Contents/MacOS/bin/orbctl"
+
+  postflight do
+    system_command "#{appdir}/OrbStack.app/Contents/MacOS/bin/orbctl",
+                   args: ["_internal", "brew-postflight"]
+  end
+
+  uninstall script: {
+    executable: "#{appdir}/OrbStack.app/Contents/MacOS/bin/orbctl",
+    args:       ["_internal", "brew-uninstall"],
+  }
+
+  zap trash: [
+        "~/.orbstack",
+        "~/Library/Caches/dev.kdrag0n.MacVirt",
+        "~/Library/HTTPStorages/dev.kdrag0n.MacVirt",
+        "~/Library/HTTPStorages/dev.kdrag0n.MacVirt.binarycookies",
+        "~/Library/Preferences/dev.kdrag0n.MacVirt.plist",
+        "~/Library/WebKit/dev.kdrag0n.MacVirt",
+        "~/Library/Saved Application State/dev.kdrag0n.MacVirt.savedState",
+      ],
+      rmdir: [
+        "~/OrbStack",
+      ]
+
+  caveats <<~EOS
+    Open the OrbStack app to finish setup.
+  EOS
+end
